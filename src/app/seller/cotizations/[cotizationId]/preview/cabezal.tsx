b38d56fd4b38a29fa34e50f965a1def07d6df18d
raphauy <rapha.uy@rapha.uy>
2025-02-12 12:10:04 -0300

    pdf logo fix

--- src/app/seller/cotizations/[cotizationId]/preview/cabezal.tsx
+++ src/app/seller/cotizations/[cotizationId]/preview/cabezal.tsx
@@ -1,25 +1,21 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
 
 export default function Cabezal() {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
   return (
     <header className="pt-6 pb-1 text-gray-800">
         <div className="h-2 bg-verde-abbate mb-4"/>
 
-        <div className="mx-auto flex justify-between items-center ">
-            <div className="flex items-center w-72 pt-4">
+        <div className="mx-auto flex justify-between items-center">
+            <div className="flex items-center w-72 min-h-[60px] pt-4">
                 <Image 
                   src="/logo4.jpeg" 
-                  width={500} 
-                  height={100} 
-                  alt="logo" 
-                  onLoad={() => setImageLoaded(true)}
-                  className={!imageLoaded ? 'opacity-0' : 'opacity-100'}
+                  width={280} 
+                  height={60} 
+                  alt="logo"
                   priority
+                  style={{ height: 'auto' }}
                 />
             </div>
             <div className="flex space-x-4 text-[10px]">
